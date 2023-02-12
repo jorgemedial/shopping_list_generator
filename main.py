190e3fb546c618746b15cfe4fab2a0d73a3da7b4
jorgemedial <jorgemedial@gmail.com>
2023-02-12 02:08:21 +0100

    Connect to dummy google sheet

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
+from gsheetsdb import connect
 
-header = st.container()
-
-with header:
-    st.title("Hello world")
+# Create a connection object.
+conn = connect()
 
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+
+sheet_url = st.secrets["dummy"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} is :{row.age}:")
